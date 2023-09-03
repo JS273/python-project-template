cd84b24e83cdeca45f5f9194d6d6c9e090750161
Julius Schultz <j.schultz@tu-braunschweig.de>
2023-09-03 12:37:04 +0200

    Added model 1b and multi col plot

--- run/public/demo_analytical_models.py
+++ run/public/demo_analytical_models.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-from demoPkg.models.analytic import analytic_model_1d, analytic_model_2d
+from demoPkg.models.analytic import analytic_model_1d, analytic_model_2d, analytic_model_1b_1d
 from demoPkg.utils.filemanager import save_script
 from demoPkg.utils.logger import init_logger
 from demoPkg.utils.argparser import get_config
@@ -24,16 +24,21 @@ config = get_config("run\intern\model_A.yaml", result_folder, logger)
 # --- Run section ---
 # -------------------
 
-# create and run model A
-my_mod = analytic_model_1d(config = config, logger = logger)
+# create and run model 1
+my_mod_1 = analytic_model_1d(config = config, logger = logger)
 x = np.linspace(0.0, 10, config.nx)
-plt_A = my_mod.calculate(x, returnType= "plot")
+plt_A = my_mod_1.calculate(x, returnType= "plot")
+
+# create and run model 1b
+my_mod_1b = analytic_model_1b_1d(config = config, logger = logger)
+x = np.linspace(0.0, 10, config.nx)
+plt_B = my_mod_1b.calculate(x, returnType= "plot")
 
 # Create and run model B
-my_mod_B = analytic_model_2d(logger = logger)
+my_mod_2 = analytic_model_2d(logger = logger)
 x_disc = np.linspace(0.0, 10, 100)
 y_disc = np.linspace(0.0, 10, 50)
-plt_B = my_mod_B.calculate(x_disc, y_disc, returnType= "plot")
+plt_C = my_mod_2.calculate(x_disc, y_disc, returnType= "plot")
 
 
 # --------------------
@@ -47,10 +52,10 @@ plotter_paper = Plotter(save_path = result_folder, stylesheet= "src\\demoPkg\\ut
 # Create plots
 plotter.plot(plt_A, filename = f'{tag}_plot1')
 
-plotter_paper.plot(plt_A, plt_B, plt_A, filename = f'{tag}_plot2', subplot_grid = [3,1])
+plotter_paper.plot(plt_A, plt_B, plt_C, filename = f'{tag}_plot2', subplot_grid = [3,1])
 
 fig, ax = get_multcolumn_subplot(n_regular_plots = 3, n_multi_colm = 1, regular_plot_grid = [1,3])
-plotter.plot(plt_A, plt_B, plt_A, plt_A, filename = f'{tag}_plot1', custom_fig = [fig, ax])
+plotter.plot(plt_A, plt_B, plt_C, plt_A, filename = f'{tag}_plot3', custom_fig = [fig, ax])
 
 
 
